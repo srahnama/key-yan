6bbe5d823c7a9035745303aca1ccb7c92c0c05d6
shahab rahnama <shahaberahnama@gmail.com>
2020-11-13 22:33:52 +0330

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+
+from view.index import index
+
+
+
+
+
+
+
+
+
+def main():
+    index()
+    print('main')
+
+
+if __name__ == "__main__":
+    main()
---
+++ view/index.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+
+
+
+
+
+
+
+
+
+
+
+def index():
+    print('index')
+
+
+if __name__ == "__main__":
+    index()
